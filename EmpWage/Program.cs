83ec764557ddfadca77f510e261de9d375bf3858
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-29 20:49:24 +0530

    [Wasim] Add: check dailywage of employee

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -10,23 +10,30 @@ namespace EmpWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage by OOPS Program");
-
+            //CONSTATNTS
             const int ISPRESENT = 1;
             const int ISABSENT = 0;
-
+            const int WAGEPERHOUR = 20;
+            const int FULLDAYHOUR = 8;
+            //VARIABLES
             int empCheck;
+            int workingHour;
 
             Random random = new Random();
             empCheck = random.Next(0, 2);
+            //Checking Absent & present & assigning workinghours.
             switch (empCheck)
             {
                 case ISPRESENT:
-                    Console.WriteLine("Employee is Present");
+                    Console.WriteLine("Employee is present");
+                    workingHour = FULLDAYHOUR;
                     break;
                 default:
-                    Console.WriteLine("Employee is Absent");
+                    Console.WriteLine("Employee is absent");
+                    workingHour = 0;
                     break;
             }
+            Console.WriteLine($"Daily EmpWage is {workingHour * WAGEPERHOUR}");
 
         }
     }
