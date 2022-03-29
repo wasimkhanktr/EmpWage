d10a5e4ab54e0c2bc36520b30ed2a689cdeda8dc
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-29 20:46:00 +0530

    [Wasim] Add: check attendence of employee

---
+++ EmpWage/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage by OOPS Program");
+
+            const int ISPRESENT = 1;
+            const int ISABSENT = 0;
+
+            int empCheck;
+
+            Random random = new Random();
+            empCheck = random.Next(0, 2);
+            switch (empCheck)
+            {
+                case ISPRESENT:
+                    Console.WriteLine("Employee is Present");
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
+            }
+
+        }
+    }
+}
